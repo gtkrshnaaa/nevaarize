b19a6186421271a9084cb40e1c88135bb330ce06
gtkrshnaaa <gtkrshnaaa@gmail.com>
2026-03-08 20:22:21 +0700

    bump: version to v0.2.3 and update documentation/reports

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -160,7 +160,7 @@ void printUsage(const char* program) {
  * Print version information.
  */
 void printVersion() {
-    std::cout << "Nevaarize v0.2.2" << std::endl;
+    std::cout << "Nevaarize v0.2.3" << std::endl;
     std::cout << "Native JIT Compiler for the Nevaarize Programming Language" << std::endl;
     std::cout << "Built with C++23, Zero External Dependencies" << std::endl;
 }
